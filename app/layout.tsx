d355ff0b8530ce05dd88698585e3a45d3cf4c119
taishikato <taishi.k0903@gmail.com>
2025-02-11 21:14:35 +0700

    Update default theme to dark in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="dark"
           disableTransitionOnChange
         >
           {children}
