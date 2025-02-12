854801ae3f07b341dbea41566a499e5e113821d7
taishikato <taishi.k0903@gmail.com>
2025-02-12 18:10:59 +0700

    delete CF things to move to Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,13 @@
-const SUPABASE_FUNCTION_URL =
-  "https://qxxlcbvvszqlusrmczke.supabase.co/functions/v1";
-// "http://127.0.0.1:54321/functions/v1";
-
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/feedback",
-        destination: `${SUPABASE_FUNCTION_URL}/feedback`,
+        destination: `${process.env.SUPABASE_FUNCTION_URL}/feedback`,
       },
       {
         source: "/api/keys",
-        destination: `${SUPABASE_FUNCTION_URL}/keys`,
+        destination: `${process.env.SUPABASE_FUNCTION_URL}/keys`,
       },
       // until Supabase Edge Functions doesn't do `CPU Time exceeded`
       // {
